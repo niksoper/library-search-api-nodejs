89d124621fec98a3e0e53a8e965a3649e97e31bc
Nick Soper <nsoper@scottlogic.com>
2017-01-14 16:33:42 +0000

    Supporting GET /available-books

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const librarySearch = require('library-search')
+
+app.route('/available-books')
+  .get(function(req, res, next) {
+    if (!req.query.title) {
+      res.status(400).json({ message: 'A title must be provided in the query string.' })
+    }
+  
+    const titleDecoded = decodeURIComponent(req.query.title)
+    librarySearch(titleDecoded)
+      .then(results => res.json(results))
+      .catch(error => {
+        res.status(500).json({ error })
+      })
+  })
+    
+const port = 80
+
+console.log(`Listening on port ${port}...`)
+
+http.createServer(app).listen(port)
