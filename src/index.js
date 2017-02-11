e513625bb7bf7e00d15c269fc491898d44d2c6f4
Nick Soper <nsoper@scottlogic.com>
2017-02-11 17:16:25 +0000

    Cache ALL responses in memory for 30 minutes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const librarySearch = require('library-search')
 
 app.set('port', process.env.PORT || 5000)
 
+const cache = require('apicache').middleware
+app.use(cache('30 minutes'))
+
+function cacheTest() {
+  console.log('middleware arguments', arguments)
+  return false
+}
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -14,8 +22,9 @@ app.route('/available-books')
   .get(function(req, res, next) {
     if (!req.query.title) {
       res.status(400).json({ message: 'A title must be provided in the query string.' })
+      return
     }
-  
+
     const titleDecoded = decodeURIComponent(req.query.title)
     librarySearch(titleDecoded)
       .then(results => res.json(results))
